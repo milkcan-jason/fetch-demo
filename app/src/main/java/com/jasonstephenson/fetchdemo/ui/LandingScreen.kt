cbd76491984024fa6f6bcc750db0dc7324d7d76d
Jason Stephenson <jason.p.stephenson@gmail.com>
2024-12-10 16:20:00 -0600

    UI tweaks for readability

--- app/src/main/java/com/jasonstephenson/fetchdemo/ui/LandingScreen.kt
+++ app/src/main/java/com/jasonstephenson/fetchdemo/ui/LandingScreen.kt
@@ -1,6 +1,8 @@
 package com.jasonstephenson.fetchdemo.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -13,12 +15,17 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.jasonstephenson.fetchdemo.data.FetchData
+import com.jasonstephenson.fetchdemo.data.FetchRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 @Composable
 fun LandingScreen(
@@ -54,12 +61,21 @@ fun LandingScreen(
                             Text(
                                 text = "ListId: $key",
                                 fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold,
                             )
                             for (fetchData in results.getValue(key)) {
-                                Text(
-                                    text = "Id: ${fetchData.id} - Name: ${fetchData.name}",
-                                    fontSize = 16.sp,
-                                )
+                                Row {
+                                    Text(
+                                        text = "Id: ${fetchData.id}",
+                                        fontSize = 20.sp,
+                                        modifier = Modifier.weight(.2f)
+                                    )
+                                    Text(
+                                        text = "Name: ${fetchData.name}",
+                                        fontSize = 20.sp,
+                                        modifier = Modifier.weight(.5f)
+                                    )
+                                }
                             }
                         }
                     }
@@ -74,5 +90,25 @@ fun LandingScreen(
 @Preview
 @Composable
 fun LandingScreen_Preview() {
-    LandingScreen()
+    class DummyRepo: FetchRepository() {
+        override suspend fun loadData(): List<FetchData>  = withContext(Dispatchers.Main) {
+            // dummy data to wire up infrastructure
+            val data: MutableList<FetchData> = mutableListOf()
+            // Count down to verify order
+            for(i in 5 downTo 1) {
+                // set #3 to a null name
+                val name = if(i%3 == 0) null else "Name $i"
+                // create data points
+                val itr = FetchData(id = i, listId = i%2, name = name)
+                data.add(itr)
+            }
+
+            return@withContext data
+        }
+    }
+
+    LandingScreen(
+        modifier = Modifier.background(Color.White),
+        viewModel = LandingScreenViewModel(DummyRepo())
+    )
 }
